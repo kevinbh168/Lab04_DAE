06d01b65d053f28d37dcbc241bb54ee15fdbe6d8
Kevin Angello Breña Huachin <kevin.brena@tecsup.edu.pe>
2020-04-21 00:15:41 -0500

    lab4

---
+++ Data/DDetallePedido.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entity;
+
+namespace Data
+{
+    public class DDetallePedido
+    {
+        public List<DetallePedido> GetDetallePedidos (DetallePedido detalle)
+        {
+            SqlParameter[] parameters = null;
+            string commandText = string.Empty;
+            List<DetallePedido> detalles = null;
+
+            try
+            {
+  
+                parameters = new SqlParameter[1];
+                parameters[0] = new SqlParameter("@IdPedido", SqlDbType.Int);
+                parameters[0].Value = detalle.Pedido.IdPedido;
+             
+
+                detalles = new List<DetallePedido>();
+
+                using (SqlDataReader reader = SqlHelper.ExecuteReader(SqlHelper.Connection, "USP_ListarDetalle",
+                    CommandType.StoredProcedure, parameters))
+                {
+                    while (reader.Read())
+                    {
+                        detalles.Add(new DetallePedido
+                        {
+                            IdProducto = reader["IdProducto"] != null ? Convert.ToInt32(reader["IdProducto"]) : 0,
+                            Cantidad = reader["Cantidad"] != null ? Convert.ToInt32(reader["Cantidad"]) :0,
+                            PrecioUnidad = reader["PrecioUnidad"] != null ? Convert.ToDecimal(reader["PrecioUnidad"]) : 0,
+                            Descuento = reader["Descuento"] != null ? Convert.ToDecimal(reader["Descuento"]) : 0             
+                        });
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return detalles;
+
+        }
+    }
+}
---
+++ Semana04_Capas/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Bussiness;
+
+namespace Semana04_Capas
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnConsultar_Click(object sender, RoutedEventArgs e)
+        {
+            BPedido bPedido = null;
+            try
+            {
+                bPedido = new BPedido();
+                DateTime fechaInicio = Convert.ToDateTime(txtFechaInicio.Text);
+                DateTime fechaFin = Convert.ToDateTime(txtFechaFin.Text);
+                dgvPedido.ItemsSource = bPedido.GetPedidosEntreFechas(fechaInicio, fechaFin);
+            }
+            catch(Exception)
+            {
+                MessageBox.Show("Comunicar con el admin");
+            }
+            finally
+            {
+                bPedido = null;
+            }
+        }
+
+        private void Grid_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DgvPedido_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            BDetallePedido bDetallePedido = null;
+            try
+            {
+                bDetallePedido = new BDetallePedido();
+                int idPedido;
+                var item = dgvPedido.SelectedItem as DataRowView;
+                if (item == null) return;
+                idPedido = Convert.ToInt32(item.Row["IdPedido"]);
+                dgvDetallePedido.ItemsSource = bDetallePedido.GetDetallePedidosPorId(idPedido);
+                txtTotal.Text = Convert.ToString(bDetallePedido.GetDetalleTotalPorId(idPedido));
+            }
+            catch(Exception)
+            {
+                MessageBox.Show("Comunicar con el admin");
+            }
+            finally
+            {
+                bDetallePedido = null;
+            }
+        }
+    }
+}
